c55bda014900d6b71ebdb88703d2dfa332476f24
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-05-23 23:41:33 +0900

    15강

--- src/main/java/com/ll/rest_api/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/rest_api/boundedContext/member/controller/MemberController.java
@@ -41,8 +41,6 @@ public class MemberController {
     public RsData<LoginReponse> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) {
         String accessToken = memberService.genAccessToken(loginRequest.getUsername(), loginRequest.getPassword());
 
-        resp.addHeader("Authentication", accessToken);
-
         return RsData.of("S-1", "액세스 토큰이 생성되었습니다", new LoginReponse(accessToken));
     }
 }
